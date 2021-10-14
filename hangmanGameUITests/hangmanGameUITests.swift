dd1caa6790502003ae11104897bbd049ab941337
Lea <lea.dukaez@gmail.com>
2021-10-14 09:59:01 +0200

    - UI test for hint fonctionnality

--- hangmanGameUITests/hangmanGameUITests.swift
+++ hangmanGameUITests/hangmanGameUITests.swift
@@ -39,9 +39,32 @@ class hangmanGameUITests: XCTestCase {
         
         XCTAssertTrue(cell0.staticTexts.element.label == "espace")
         XCTAssertTrue(cell1.staticTexts.element.label == "animaux")
+    }
+    
+    func test_working_Hint() {
+        let app = XCUIApplication()
+        app.launch()
         
+        let cell0 = app.tables["ListOfThemes"].cells["themeCell_0"]
         cell0.tap()
+    
+        let navBarTitle = app.navigationBars.staticTexts.element.label
+        XCTAssertTrue(navBarTitle == "espace")
         
+        app.navigationBars["espace"].buttons["lightbulb"].tap()
+        XCTAssertTrue(app.alerts["Indice"].exists)
+
+        let messageIndice = app.alerts["Indice"].staticTexts.element(boundBy: 1).label
+        let allActualSpaceTexts = """
+EPOQUE: Point déterminé dans le temps, marquant le début d'une ère.
+LUSTRE: Période de temps longue et indéterminée.
+SIECLE: Période de cent années environ, considérée comme une unité historique.
+ETOILE: Astre producteur et émetteur d'énergie.
+ATMOSPHERE: Couche gazeuse qui entoure le globe terrestre.
+GALAXIE: Vaste amas d'étoiles, l'une des structures essentielles de l'Univers.
+"""
+        XCTAssertTrue(allActualSpaceTexts.contains(messageIndice))
+        app.alerts["Indice"].buttons["Retour"].tap()
     }
 
     func testLaunchPerformance() throws {
